ce09f99174e38b17bd6f047dc43b19f4f3612334
Pedro Laurenti <pedrolaurenti04@gmail.com>
2024-04-25 09:01:08 -0300

    20.7

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -25,32 +25,34 @@ export default function Grid() {
       <div className={styles.parte3back2}></div>
       <div className={styles.parte3wrapper}>
         <h1>O que oferecemos?</h1>
-        <div className={styles.wrapperCellsPt3}>
-          {elements.map((element, index) => (
-            <div key={index} className={styles.cellPt3}>
-              <div
-                className={styles.ImgDad1}
-                style={{
-                  borderColor: colors[(currentColorIndex + index) % colors.length],
-                  border: `2px solid ${colors[(currentColorIndex + index) % colors.length]}`
-                }}
-              >
-                <div className={styles.WrapperImgPt3}>
-                  <Image
-                    className={styles.imges}
-                    src={element.imageSrc}
-                    width={300}
-                    height={300}
-                    alt="a"
-                    quality={80}
-                    sizes="30vw"
-                    draggable="false"
-                  />
+        <div className={styles.wrapperwrapper}>
+          <div className={styles.wrapperCellsPt3}>
+            {elements.map((element, index) => (
+              <div key={index} className={styles.cellPt3}>
+                <div
+                  className={styles.ImgDad1}
+                  style={{
+                    borderColor: colors[(currentColorIndex + index) % colors.length],
+                    border: `2px solid ${colors[(currentColorIndex + index) % colors.length]}`
+                  }}
+                >
+                  <div className={styles.WrapperImgPt3}>
+                    <Image
+                      className={styles.imges}
+                      src={element.imageSrc}
+                      width={300}
+                      height={300}
+                      alt="a"
+                      quality={80}
+                      sizes="30vw"
+                      draggable="false"
+                    />
+                  </div>
                 </div>
+                <h3 style={{ color: colors[(currentColorIndex + index) % colors.length] }}>{element.title}</h3>
               </div>
-              <h3 style={{ color: colors[(currentColorIndex + index) % colors.length] }}>{element.title}</h3>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
